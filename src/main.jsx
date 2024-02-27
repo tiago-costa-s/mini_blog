525615f58bd9abd59d0bd17267fac790cf749b40
tiago-costa-s <tiagocostasantos0013@gmail.com>
2024-02-27 20:59:32 -0300

    Router-Dom configurações iniciais

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './routes/home/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/routes/errorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import './ErrorPage.css'
+
+const ErrorPage = () => {
+
+    return (
+        <div>
+            <h1>Página de Erro</h1>
+        </div>
+    );
+
+};
+
+export default ErrorPage;
