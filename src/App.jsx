d2ef24409633490d43259a331962d31339ab6db9
tiago-costa-s <tiagocostasantos0013@gmail.com>
2024-02-28 20:10:34 -0300

    Criação do componente NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import NavBar from './components/navBar/NavBar';
 
 function App() {
 
   return (
-    <div className="App">    
+    <div className="App">
+      <NavBar />
       <Outlet />
     </div>
   );
