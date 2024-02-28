1a99fad41a2a3535faa044005f32e3d217b576cf
tiago-costa-s <tiagocostasantos0013@gmail.com>
2024-02-28 20:29:05 -0300

    Estilização do Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import './Footer.css';
 
 const Footer = () => {
 
     return (
-        <footer>
+        <footer className='footer'>
             <h3>Escreva sobre o que você tem interesse!</h3>
             <p>mini blog &copy; 2022</p>
         </footer>
