5bf698bdc443b5856747f8a8bdcdf1003664a7dd
tiago-costa-s <tiagocostasantos0013@gmail.com>
2024-02-28 20:12:49 -0300

    Criação do componente Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import NavBar from './components/navBar/NavBar';
+import Footer from './components/footer/Footer';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Outlet />
+      <Footer />
     </div>
   );
 
